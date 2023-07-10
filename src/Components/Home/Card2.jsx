29df288781c24b9f2e130e7fa3d3a5aaeb6748a5
therealmensumit <sk03950@gmail.com>
2023-07-10 09:38:33 +0530

    adjust space

--- src/Components/Home/Card2.jsx
+++ src/Components/Home/Card2.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Card2 = (props) => {
   return (
-    <div className='col-span-4 md:col-span-2 lg:col-span-1 rounded-3xl border-2 border-white overflow-hidden even:mt-12 odd:self-start group'>
+    <div className='col-span-4 md:col-span-2 lg:col-span-1 rounded-3xl border-2 border-white overflow-hidden lg:even:mt-12 lg:odd:self-start group'>
         <img className="w-full rounded-xl group-hover:scale-110 transition-all" src={props.img} alt={props.alt} />
         <div className="p-4">
             <h3 className="font-head text-white text-2xl font-semibold mb-1">{props.title}</h3>
